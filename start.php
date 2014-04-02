d7a6d18d8b01d41ba93bcce8d0ddb23951382e28
Aleksandrs Poltarjonoks <apoltarjonoks@gmail.com>
2014-04-02 22:30:34 +0200

    У тебя вопрос есть?

--- start.php
+++ start.php
@@ -2,6 +2,7 @@
  $sunduk = [];
  $sunduk =[ [23 =>[ [12 => 'SLOVO'],
                     [10 => 'netu SLOVa']]],
+                    //tut tak ne nado
 
             [24 => 'imja']];
 // comentarij
